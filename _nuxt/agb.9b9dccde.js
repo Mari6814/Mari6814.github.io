b3a338a6005d6a9bbdd9f55837a9ee77f377ea7b
Marian Plivelic <MarianPlivelic@gmail.com>
2024-02-25 19:42:50 +0100

    add gap for large screens

---
+++ _nuxt/agb.9b9dccde.js
@@ -0,0 +1,1 @@
+import{u as e}from"./index.8a9dbdb5.js";import{f as n}from"./entry.ff907a44.js";import"./vue.f36acd1f.05572ffc.js";const g=n({__name:"agb",setup(t){return e({title:"AGB",description:"Die Allgemeinen Geschäftsbedingungen von Grünig Kleber",ogTitle:"AGB",ogDescription:"Die Allgemeinen Geschäftsbedingungen von Grünig Kleber"}),(i,o)=>" Under construction "}});export{g as default};
