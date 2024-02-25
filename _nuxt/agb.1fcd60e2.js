50176fa67b94d142dededd254596b29ae4f03b30
Marian Plivelic <MarianPlivelic@gmail.com>
2024-02-25 19:15:56 +0100

    apply contact text feedback

---
+++ _nuxt/agb.1fcd60e2.js
@@ -0,0 +1,1 @@
+import{u as e}from"./index.0dacc552.js";import{f as n}from"./entry.c9a6c7bb.js";import"./vue.f36acd1f.18f6e41a.js";const g=n({__name:"agb",setup(t){return e({title:"AGB",description:"Die Allgemeinen Geschäftsbedingungen von Grünig Kleber",ogTitle:"AGB",ogDescription:"Die Allgemeinen Geschäftsbedingungen von Grünig Kleber"}),(i,o)=>" Under construction "}});export{g as default};
