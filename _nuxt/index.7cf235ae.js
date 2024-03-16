3aa48f9c2791bbad920d477ce22575b658fe070e
Marian Plivelic <MarianPlivelic@gmail.com>
2024-03-16 18:13:47 +0100

    adjust logo size

---
+++ _nuxt/index.7cf235ae.js
@@ -0,0 +1,1 @@
+import{u as n}from"./vue.f36acd1f.6da2c0bf.js";import{u as s}from"./entry.8655abe2.js";function i(e,a){const{title:r,titleTemplate:o,...m}=e;return n({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const u=s({...t._flatMeta});return delete t._flatMeta,{...t,meta:u}}})}export{i as u};
