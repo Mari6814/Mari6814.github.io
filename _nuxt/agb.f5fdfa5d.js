f7d3fa264285888bbdd2520616c0ee3bc87e0c8c
Marian Plivelic <MarianPlivelic@gmail.com>
2024-03-16 17:55:50 +0100

    fix overflow when switching loesungen tab

---
+++ _nuxt/agb.f5fdfa5d.js
@@ -0,0 +1,1 @@
+import{u as e}from"./index.14b3c2fa.js";import{f as n}from"./entry.bafc7295.js";import"./vue.f36acd1f.eb39644b.js";const g=n({__name:"agb",setup(t){return e({title:"AGB",description:"Die Allgemeinen Geschäftsbedingungen von Grünig Kleber",ogTitle:"AGB",ogDescription:"Die Allgemeinen Geschäftsbedingungen von Grünig Kleber"}),(i,o)=>" Under construction "}});export{g as default};
