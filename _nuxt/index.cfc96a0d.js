1d3451d9a0b4891f8552134be08166d5c1cb1b5f
Marian Plivelic <MarianPlivelic@gmail.com>
2024-03-16 18:07:41 +0100

    adjust address column width

---
+++ _nuxt/index.cfc96a0d.js
@@ -0,0 +1,1 @@
+import{u as n}from"./vue.f36acd1f.80416c2b.js";import{u as s}from"./entry.9eccb781.js";function i(e,a){const{title:r,titleTemplate:o,...m}=e;return n({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const u=s({...t._flatMeta});return delete t._flatMeta,{...t,meta:u}}})}export{i as u};
