1df55232a126125e2f1eff5802a52d9596a5e37b
Marian Plivelic <MarianPlivelic@gmail.com>
2024-03-16 18:19:06 +0100

    fix index floating section for intermediate sceen size

---
+++ _nuxt/agb.46362d1e.js
@@ -0,0 +1,1 @@
+import{u as e}from"./index.2b96477e.js";import{f as n}from"./entry.9fb9fd1b.js";import"./vue.f36acd1f.9a0ce3b0.js";const g=n({__name:"agb",setup(t){return e({title:"AGB",description:"Die Allgemeinen Geschäftsbedingungen von Grünig Kleber",ogTitle:"AGB",ogDescription:"Die Allgemeinen Geschäftsbedingungen von Grünig Kleber"}),(i,o)=>" Under construction "}});export{g as default};
